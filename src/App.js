337bfae5631d772168960f261feabe63a81036df
Hitesh Parmar <10hiteshparmar@gmail.com>
2023-06-14 20:10:38 +0530

    ading custom component for a card. Created files for further changes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import SearchGui from './components/SearchGui';
 
 function App() {
   return (
     <div className="App">
+      <SearchGui />
     </div>
   );
 }
---
+++ src/components/SearchGui.js
@@ -0,0 +1,12 @@
+import Card from "../UI/Card";
+import "./SearchGui.css"
+
+const SearchGui = () => {
+    return (
+        <Card>
+            <div className="title">Search Bar</div>
+        </Card>
+    )
+}
+
+export default SearchGui;
