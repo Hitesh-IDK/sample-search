3683c85c0b4211363d0d4bcc20def6ced4e47eb6
Hitesh Parmar <10hiteshparmar@gmail.com>
2023-06-20 15:35:55 +0530

    infinite scroll feature replacing the buttons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import SearchGui from './components/SearchGui';
 
 function App() {
-  // const scrollHandler = (event) => {
-  //   console.log
-  //   console.log(event.target)
-  // }
+
+  
 
   return (
     <div className="App" >
